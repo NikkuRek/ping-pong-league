937ac27da2783da115a92d68b21c534db4d1db3d
Edgarbriceno26@outlook.es <Edgarbriceno26@outlook.es>
2025-04-26 11:40:34 -0400

    ya se puede hacer el post del jugador

--- src/js/registro.js
+++ src/js/registro.js
@@ -14,7 +14,6 @@ async function getTier(){
     try {
         const response = await axios.get('http://localhost:3000/api/tier');
         tiers = response.data.data;
-        console.log(tiers);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
@@ -56,7 +55,6 @@ async function cargarTier() {
 
     select.innerHTML = ''; // Limpia las opciones existentes
 
-    console.log(tiers);
     if (tiers.length === 0) {
         console.warn("No se encontraron tiers en la respuesta");
         return;
@@ -98,7 +96,46 @@ document.getElementById('register').addEventListener('submit', async (event) => 
     const formData = new FormData(event.target); // Obtiene los datos del formulario
     const data = Object.fromEntries(formData.entries()); // Convierte FormData a un objeto
 
-    console.log(data); // Muestra los datos en la consola para depuración
+    // Seleccionar todos los elementos tipo checkbox
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    // Crear un arreglo para almacenar los checkboxes activos
+    const checkboxesActivos = [];
+
+    // Recorrer cada checkbox y verificar si está marcado y guardarlo en el arreglo
+    checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+        checkboxesActivos.push(checkbox.value);
+    }
+    });
+
+    // Mostrar los valores de los checkboxes activos
+    console.log(checkboxesActivos);
+
+    let playerData = {
+        playerData: {
+            CI: data['player-CI'],
+            first_name: data['player-name'],
+            last_name: data['player-lastname'],
+            phone: data['player-phone'],
+            semester: data['player-semester'],
+            id_career: data['player-career'],
+            id_tier: data['player-tier'],
+            status: true, // Cambia esto según tu lógica
+        },
+        available_days: checkboxesActivos  // Aquí se asigna el arreglo de checkboxes activos
+    };
+
+    console.log(playerData); // Muestra el objeto en la consola para verificar su contenido
+
+    // Enviar los datos al servidor
+    axios.post('http://localhost:3000/api/player', playerData)
+        .then((response) => {
+            console.log('Datos enviados exitosamente:', response.data);
+        })
+        .catch((error) => {
+            console.error('Error al enviar los datos:', error);
+        });
 });
 // Limpiar el formulario después de enviar los datos
-document.getElementById('register').reset(); // Limpia el formulario después de enviar los datos
+//document.getElementById('register').reset(); // Limpia el formulario después de enviar los datos
