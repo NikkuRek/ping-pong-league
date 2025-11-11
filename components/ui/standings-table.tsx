8fc25766e040bee596589e6717a4367e97ecad20
Nikku <yoks.rar@gmail.com>
2025-11-10 20:52:38 -0400

    av

--- components/ui/standings-table.tsx
+++ components/ui/standings-table.tsx
@@ -24,11 +24,11 @@ export function StandingsTable({ standings }: StandingsTableProps) {
                         qualifiedSpots = 16
                         almostQualifiedSpots = 0
                     } else if (totalParticipants >= 16) {
-                        qualifiedSpots = 4
-                        almostQualifiedSpots = 6
+                        qualifiedSpots = 8
+                        almostQualifiedSpots = 0
                     } else if (totalParticipants >= 14) {
-                        qualifiedSpots = 6
-                        almostQualifiedSpots = 4
+                        qualifiedSpots = 12
+                        almostQualifiedSpots = 0
                     } else if (totalParticipants >= 12) {
                         qualifiedSpots = 8
                         almostQualifiedSpots = 0
